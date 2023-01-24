46e62491bdb2def55e09a723d9c2c9f5374b835f
Ilya Kulebayev <monah58rus@rambler.ru>
2023-01-24 21:29:46 +0300

    Update telbot.py

--- telbot.py
+++ telbot.py
@@ -4,17 +4,15 @@ import telebot
 
 bot = telebot.TeleBot('5647135917:AAEQQlyHm63cwX62hrC6u-QGg-EHeXkATxI')
 
-word = ['пидор','бот']
-first = (['Иди ка в хуй нарядись, ',
-         'И нахуй ты вообще родился, ',
-         'А ты хорош, но я все равно твою мать ебал... '])
+word = []
+first = []
 second = ['21', '22', '23']
 third = ['31', '32', '33']
 
 @bot.message_handler(commands=['start',]) 
 def start(message):
     if message.from_user.first_name == 'Михаил':
-        return bot.send_message(message.chat.id, 'привет пирожочек =)')
+        return bot.send_message(message.chat.id, 'привет =)')
     else:
         mess = f'Привет, {message.from_user.first_name}'
         return bot.send_message(message.chat.id, mess)
@@ -22,7 +20,7 @@ def start(message):
 
 @bot.message_handler(commands=['debug'])
 def start(message):
-    mess = f'Хуй тебе, а не отладка, {message.from_user.first_name}' 
+    mess = f'сообщение об отладке , {message.from_user.first_name}' 
     bot.send_message(message.chat.id, mess)
 
 
@@ -31,14 +29,14 @@ def start(message):
     mess = f'Сорян, но тебе я не помогу, {message.from_user.first_name}' 
     bot.send_message(message.chat.id, mess)
     time.sleep(random.randint(2, 5))
-    mess = f'Да я рофлю, вот мои команды:'
+    mess = f'вот мои команды:'
     bot.send_message(message.chat.id, mess)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
     mess = message.text.lower()
-    if 'пидор' in mess.split() and ('бот,' in mess.split() or 'бот' in mess.split()):
+    if 'помощь' in mess.split() and ('бот,' in mess.split() or 'бот' in mess.split()):
         return bot.send_message(message.chat.id, f'{random.choice(first)}{message.from_user.first_name}')
 
     if 'бот' in mess.split():
